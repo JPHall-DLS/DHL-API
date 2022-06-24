1a43e1a5345ac83a59e3ba4e64da8c8c4ed9d97b
James Hall <41731916+JPHall-DLS@users.noreply.github.com>
2022-06-24 01:59:38 +0100

    Replace short array syntax for PHP 5 compatibility

--- src/Datatype/AM/Label.php
+++ src/Datatype/AM/Label.php
@@ -19,35 +19,35 @@ class Label extends Base
      * Parameters of the datatype
      * @var array
      */
-    protected $params = [
-        'LabelTemplate' => [
+    protected $params = array(
+        'LabelTemplate' => array(
             'type' => 'LabelTemplate',
             'required' => false,
             'subobject' => false,
             'comment' => 'LabelTemplate',
             'enumeration' => '8X4_A4_PDF,8X4_thermal,8X4_PDF,8X4_A4_TC_PDF,6X4_A4_PDF,6X4_thermal,'
                 . '8X4_CI_PDF,8X4_CI_thermal',
-        ],
-        'Logo' => [
+        ),
+        'Logo' => array(
             'type' => 'YesNo',
             'required' => false,
             'subobject' => false,
             'comment' => 'Boolean flag',
             'length' => '1',
             'enumeration' => 'Y,N',
-        ],
-        'CustomerLogo' => [
+        ),
+        'CustomerLogo' => array(
             'type' => 'CustomerLogo',
             'required' => false,
             'subobject' => true,
-        ],
-        'Resolution' => [
+        ),
+        'Resolution' => array(
             'type' => 'Resolution',
             'required' => false,
             'subobject' => false,
             'comment' => 'Resolution',
             'minInclusive' => '200',
             'maxInclusive' => '300',
-        ],
-    ];
+        ),
+    );
 }
